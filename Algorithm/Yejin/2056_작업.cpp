d79585126d962ee42af4ed8759435d0fc6e1a0d3
Jinnie <68051794+rocher71@users.noreply.github.com>
2025-02-18 17:41:50 +0900

    위상정렬 '작업' 문제 update

--- Algorithm/Yejin/2056_작업.cpp
+++ Algorithm/Yejin/2056_작업.cpp
@@ -1,3 +1,5 @@
+// 이 문제는 선행 task가 무조건 현재 task보다 작다는 조건이 있기 때문에, 
+// 입력을 받으면서도 처리를 할 수 있는거임. 문제 꼼꼼히 읽기.
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -26,7 +28,7 @@ int main() {
 			continue;
 		}
 
-		// ���� ���谡 2�� �̻��� ���
+		// 선행 관계가 2개 이상인 경우
 		vector<int> preTimes(preCnt);
 		for (int j = 0; j < preCnt; j++) {
 			int pre;
