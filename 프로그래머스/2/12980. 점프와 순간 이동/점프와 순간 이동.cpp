b7f69c1571740733188710940d8b5b35b481e074
Jinnie <68051794+rocher71@users.noreply.github.com>
2025-04-02 12:12:18 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 3.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<climits>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    
+    while(n != 0){
+        if(n % 2 == 0){
+            n /= 2;
+            continue;
+        }
+        ans++;
+        n -= 1;
+    }
+    
+    
+    return ans;
+}
