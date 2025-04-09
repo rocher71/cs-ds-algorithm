d9bbcae38ab61d6f769c32b92cd76aa6adffeec8
Jinnie <68051794+rocher71@users.noreply.github.com>
2025-04-09 12:25:09 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,13 +1,10 @@
 #include<iostream>
-#include<vector>
 #include<algorithm>
 #include<stack>
 using namespace std;
 
 int n, maxLen = 1;
-int arr[1001], dp[1001];
-vector<int> lis;
-vector<int> nums[1001];
+int arr[1001], dp[1001]; //dp[i] = b : arr[i]값으로 끝나는 가장 긴 증가하는 수열의 길이
 stack<int> st;
 
 int main() {
@@ -18,18 +15,19 @@ int main() {
 	cin >> n;
 	for (int i = 0; i < n; i++) cin >> arr[i];
 
+	
 	dp[0] = 1;
 	for (int i = 1; i < n; i++) {
 		for (int j = 0; j < i; j++) {
+			if(dp[i] == 0) dp[i] = 1;
 			if (arr[j] < arr[i]) dp[i] = max(dp[i], dp[j] + 1);
 			maxLen = max(maxLen, dp[i]);
 		}
-		if (dp[i] == 0) dp[i] = 1;
 	}
 	cout << maxLen << "\n";
 
+	// dp배열을 돌며 가장 긴 증가하는 수열의 가장 큰 값부터 순차적으로 스택에 쌓아줌
 	for (int i = n - 1; i >= 0; i--) {
-		
 		if (dp[i] != maxLen) continue;
 		st.push(arr[i]);
 		maxLen--;
