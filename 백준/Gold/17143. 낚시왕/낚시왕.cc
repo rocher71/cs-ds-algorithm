5ac1bba90b23ce0b05d16114fd5feb67d2523960
김예진 <68051794+rocher71@users.noreply.github.com>
2025-12-10 21:59:45 +0900

    Refactor shark movement and catching logic

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -148,4 +148,149 @@ int main(){
 
     cout << caughtSharkSum << "\n";
 
+
+}
+
+// 두번재 풀이
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<algorithm>
+using namespace std;
+
+int row, col, sharkCnt;
+unordered_map<int, vector<int>> sharks;
+int arr[101][101], times[101][101];
+unordered_set<int> removedShark;
+// 위, 아래, 오른쪽, 왼쪽
+int dy[5] = {0, -1, 1, 0, 0}, dx[5] = {0, 0, 0, 1, -1};
+
+int catchNearestShark(int x){
+    for(int i = 1; i<= row; i++){
+        // 빈 칸이거나 이미 벅힌/잡힌 상어면 continue
+        if(!arr[i][x] || removedShark.find(arr[i][x]) != removedShark.end()) continue;
+        removedShark.insert(arr[i][x]);
+        return sharks[arr[i][x]][4];
+    }
+    return 0;
+}
+
+int changeDirection(int currDir){
+    if(currDir == 1) return 2;
+    if(currDir == 2) return 1;
+    if(currDir == 3) return 4;
+    return 3;
+}
+
+void moveShark(int sharkNum, int time){
+    int y = sharks[sharkNum][0];
+    int x = sharks[sharkNum][1];
+    int speed = sharks[sharkNum][2];
+    int dir = sharks[sharkNum][3];
+    int size = sharks[sharkNum][4];
+
+    int currY = y, currX = x, currDir = dir;
+    for(int i = 0; i < speed; i++){
+        int newY = currY + dy[currDir], newX = currX + dx[currDir];
+
+        // 다음 칸이 범위 내면 continue
+        if(newY > 0 && newY <= row && newX > 0 && newX <= col){
+            currY = newY;
+            currX = newX;
+            continue;
+        }
+
+        // 그렇지 않다면 방향 전환 후 한칸 전진
+        currDir = changeDirection(currDir);
+        currY += dy[currDir];
+        currX += dx[currDir];
+    }
+
+    // 이전 자리에서 상어 없애기, 단 이미 이번 턴에 새로운 상어가 들어왔을 수 있으므로 현재 상어 번호와 대조 후 삭제
+    if(arr[y][x] == sharkNum){
+        arr[y][x] = 0;
+    }
+
+    int sharkInNewLoc = arr[currY][currX];
+    // 새로운 칸에 있는 상어가 더 큰 경우 (내가 잡아 먹힘)
+    if(sharkInNewLoc && times[currY][currX] == time && sharks[sharkInNewLoc][4] > size){
+        removedShark.insert(sharkNum);
+        return;
+    }
+
+    // 새로운 칸에 있는 상어가 현재 턴에 옮겨졌고 나보다 작은 경우 (내가 잡아 먹음)
+    if(sharkInNewLoc && times[currY][currX] == time && sharks[sharkInNewLoc][4] < size){
+        removedShark.insert(sharkInNewLoc);
+    }
+
+    /*
+     * 새로운 자리에
+     * 1. 상어가 없거나
+     * 2. 있는 상어가 지워진 상어거나
+     * 3. 이전 턴의 상어라면
+     * 4. 현재 턴의 상어인데 나보다 작은 경우
+     * 현재 상어 위치시키기
+     */
+    arr[currY][currX] = sharkNum;
+    times[currY][currX] = time;
+    sharks[sharkNum] = {currY, currX, speed, currDir, size};
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> row >> col >> sharkCnt;
+    if(sharkCnt == 0){
+        cout << "0\n";
+        return 0;
+    }
+    for(int i = 1; i <= sharkCnt; i++){
+        int y, x, speed, direction, size;
+        cin >> y >> x >> speed >> direction >> size;
+
+        // 세로로 움직이는 범위는 방향까지 고려하여 2(row-1), 가로는 2(col-1), 따라서 최소한의 속력만 남김
+        int minSpeed = speed;
+        if(direction == 1 || direction == 2) minSpeed = speed % (2 * (row - 1));
+        else minSpeed = speed % (2 * (col - 1));
+
+        sharks.insert({i, {y, x, minSpeed, direction, size}});
+        arr[y][x] = i;
+        times[y][x] = 0;
+    }
+
+//    for(int j = 1; j <= row; j++){
+//        for(int k = 1; k <= col; k++){
+//            if(!arr[j][k]) cout << "0 ";
+//            else cout << sharks[arr[j][k]][4] << " ";
+//        }
+//        cout << "\n";
+//    }
+//    cout << "\n";
+
+    int caughtSum = 0;
+    for(int i = 1; i <= col; i++){
+        // 낚시왕이 땅과 가장 가까운 상어를 잡음
+        caughtSum += catchNearestShark(i);
+//        cout << i << " " << caughtSum << "\n";
+
+        for(auto shark : sharks){
+            // 잡히거나 먹힌 상어라면 continue
+            if(removedShark.find(shark.first) != removedShark.end()) continue;
+            moveShark(shark.first, i);
+        }
+
+//        for(int j = 1; j <= row; j++){
+//            for(int k = 1; k <= col; k++){
+//                if(!arr[j][k]) cout << "0 ";
+//                else cout << sharks[arr[j][k]][4] << " ";
+//            }
+//            cout << "\n";
+//        }
+//        cout << "\n";
+    }
+
+    cout << caughtSum << "\n";
 }
